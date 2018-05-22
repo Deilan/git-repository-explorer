4cc9d2b22251fe1d70f113ca97f82ecc47075a7f
Deilan <deilan@mail.ru>
2018-05-22 20:05:03 +0300

    fix branch.test

--- git/branch.test.js
+++ git/branch.test.js
@@ -6,9 +6,8 @@ const chai = require('chai');
 const expect = chai.expect;
 
 describe('getBranches', function () {
-  it('содержит [\'master\', \'dev\']', async function () {
+  it('содержит [\'master\']', async function () {
     const branches = await getBranches();
     expect(branches).to.include('master');
-    expect(branches).to.include('dev');
   });
 });
